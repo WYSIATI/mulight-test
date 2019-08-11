6a2607225513c6930d35373334c6332dd9f3b7af
WYSIATI <chester.lee.cold@gmail.com>
2019-08-11 18:44:21 +0800

    Added Watch model.

--- mulight/watch/models.py
+++ mulight/watch/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Watch(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False, default=0)
+    discount_base = models.PositiveIntegerField(blank=True, null=True)
+    discount_amount = models.PositiveIntegerField(blank=True, null=True)
